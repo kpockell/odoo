57f0240aadaa0381306d74c16e7bf5064eb44f42
Kris Pockell <kpockell@bitelixir.co>
2026-04-28 06:42:55 -0700

    Removed theme_init

--- addons/web/static/src/core/theme/theme_init.js
+++ addons/web/static/src/core/theme/theme_init.js
@@ -1,18 +1,4 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
-import registerThemeMenuItem from "@web/core/theme/theme_menu_item";
-import { startService } from "@web/core/service_hook";
-import { themeService } from "@web/core/theme/theme";
-
-// This ensures the theme service is loaded and started
-startService(themeService);
-
-// Add to main services initialization
-registry.category("main_components").add("theme_service_loader", {
-    Component: class ThemeLoader extends null {
-        setup() {
-            // This is just a placeholder component to ensure the theme service is started
-        }
-    },
-});
+import "@web/core/theme/theme";
+import "@web/core/theme/theme_menu_item";
